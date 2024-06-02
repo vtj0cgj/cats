c5e2112730c7a407ff4a47ab951d4417bea57d44
VTJ0cGJ <vtj0cgj@outlook.com>
2024-06-03 09:33:28 +1000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod decrypt;
+mod encrypt;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("welcome to cats, which is an acronym for something i forgot.");
+    println!();
+    // Get the command-line arguments
+    let args: Vec<String> = env::args().collect();
+    let eord: &str = &args[1];
+
+    let password: &str = &args[2];
+    let directory: &str = &args[3];
+
+    if eord == "-e" {
+        // Encrypt directory
+        if let Err(err) = encrypt_directory_recursive(directory, password) {
+            eprintln!("Error: {}", err);
+            std::process::exit(1);
+        }
+    }
+    else if eord == "-d" {
+        //  Decrypt directory
+        if let Err(err) = decrypt_directory_recursive(directory, password) {
+            eprintln!("Error: {}", err);
+            std::process::exit(1);
+        }
+    }
+
+    
 }
