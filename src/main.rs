6cb77c295000a9dc10f8c7211efad5daeb3f631a
VTJ0cGJ <vtj0cgj@outlook.com>
2024-06-25 10:41:41 +1000

    nothing in particular

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod decrypt;
 mod encrypt;
-use std::env;
 use crate::decrypt::decrypt_directory_recursive;
 use crate::encrypt::encrypt_directory_recursive;
+use std::env;
 
 fn main() {
     println!("welcome to cats, which is an acronym for something i forgot.");
@@ -20,16 +20,19 @@ fn main() {
             eprintln!("Error: {}", err);
             std::process::exit(1);
         }
-        println!("Encryption of\ndirectory: [{}]\npasssword: [{}]\nresult: sucessful", directory, password);
-    }
-    else if eord == "-d" {
+        println!(
+            "Encryption of\ndirectory: [{}]\npasssword: [{}]\nresult: sucessful",
+            directory, password
+        );
+    } else if eord == "-d" {
         //  Decrypt directory
         if let Err(err) = decrypt_directory_recursive(directory, password) {
             eprintln!("Error: {}", err);
             std::process::exit(1);
         }
-        println!("Decryption of\ndirectory: [{}]\npasssword: [{}]\nresult: sucessful", directory, password);
+        println!(
+            "Decryption of\ndirectory: [{}]\npasssword: [{}]\nresult: sucessful",
+            directory, password
+        );
     }
-
-    
 }
